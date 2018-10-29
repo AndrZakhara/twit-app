51d00b912d83967c0a0ba2b1dc29721c71d15a2e
AndrZakhara <art.pruttov@gmail.com>
2018-10-29 21:03:22 +0200

    connect form to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { loginFormReducer } from './loginForm';
+import { reducer as formReducer } from 'redux-form';
 
 export const rootReducer = combineReducers({
   loginForm: loginFormReducer,
+  form: formReducer,
 });
