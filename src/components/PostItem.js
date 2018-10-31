b42cbb2d5a37453ecf4930d39c78738e7d5dd9c8
Andr Zakhara <art.pruttov@gmail.com>
2018-10-31 16:20:15 +0200

    add show post by id

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -4,12 +4,26 @@ export default function CategoryItem(props) {
   const {
     post,
     getPostId,
+    id,
   } = props;
 
-  return(
+  const postByPostProps = (
     <div onClick={()=>getPostId(post.id)}>
       <h4>{post.title}</h4>
       <p>{post.body}</p>
     </div>
   );
+
+  const postById = (
+    <div>
+      <h4>{post.title}</h4>
+      <p>{post.body}</p>
+    </div>
+  );
+
+  if (post !== undefined) {
+    return postByPostProps;
+  } else if (id !== undefined) {
+    return postById;
+  }
 }
